5dd07aef1614c8b5c15254db58d5d9434e38d723
galamo <galamouyal88@gmail.com>
2020-06-22 20:50:47 +0300

    feat-card-view-list-view-button

--- cars.js
+++ cars.js
@@ -1,6 +1,14 @@
 const colors = ["red", "green", "yellow", "black"];
 const types = ["BMW", "MRCDS", "Mazda", "Subaro"];
 const doors = [2, 4, 5];
+const DOM = {}
+
+const displayFunctions = {
+    "cards": getCardItem,
+    "list": getListItem,
+    "table": getRowItem,
+};
+console.log(displayFunctions)
 
 
 function generateCars(numberOfCars, isArray) { //return array with Cars ( each car is an object in JS)
@@ -51,24 +59,46 @@ function generateSingleCar(index) {
 // map - return partial result
 // reduce - next time..
 
+
 (function () {
-
     const cars = generateCars(100, true)
-    draw(cars);
+    DOM.listData = document.getElementById("data");
+    draw(cars, DOM.listData, "list");
 
-
+    const listViewButton = document.getElementById("listView");
+    const cardViewButton = document.getElementById("cardView");
 }())
 
 
-function draw(data) {
+function draw(data, domContainer, displayType) {
     if (!Array.isArray(data)) return;
-
+    if (typeof domContainer !== 'object') return;
+    const displayFunction = displayFunctions[displayType]
+    if (typeof displayFunction !== 'function') return;
     data.forEach(car => {
-        console.log(car)
+        domContainer.append(displayFunction(car))
     });
 }
 
-    
+function getListItem(carData) {
+    const listItem = document.createElement("li");
+    listItem.classList.add("list-group-item");
+    listItem.innerText = `car lp: ${carData.lp}, car color: ${carData.color}`;
+    return listItem;
+}
+
+function getCardItem(carData) {
+    const card = document.createElement("div");
+    card.style.border = "1px solid black";
+    card.style.height = "50px";
+    card.style.width = "300px";
+    card.style.display = "inline-block";
+    card.innerText = `car lp: ${carData.lp}, car color: ${carData.color}`;
+    return card;
+}
+function getRowItem() { }
+
+
 // draw
 // single list item
 // 
